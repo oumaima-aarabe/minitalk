b7d5b1a6cc2c27ec4e70d621b7c2d34b5d944dca
Oumaima Aarabe <ouaarabe@e1r13p5.1337.ma>
2023-02-27 00:30:23 +0100

    fixed the bug

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: ouaarabe <ouaarabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:15:16 by ouaarabe          #+#    #+#             */
-/*   Updated: 2023/02/26 08:24:58 by ouaarabe         ###   ########.fr       */
+/*   Updated: 2023/02/26 23:49:18 by ouaarabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,69 @@
 
 int     check_errors(int argc, char **argv)
 {
-    int i = 0;
-    if (argc != 3)
-    {
-       ft_printf("usage : ./client [pid] [message]!!! \n");
-        return 1;
-    }
-    while (argv[1][i])
-    {
-        if (!ft_strchr("0123456789", argv[1][i]))
-        {
-            ft_printf("The PID of an individual process should be an integer with a positive value! \n");
-            return (1);
-        }
-        i++;
-    }
-    return (0);
+	int i = 0;
+	if (argc != 3)
+	{
+	   ft_printf("usage : ./client [pid] [message]!!! \n");
+		return 1;
+	}
+	while (argv[1][i])
+	{
+		if (!ft_strchr("0123456789", argv[1][i]))
+		{
+			ft_printf("The PID of an individual process should be an integer with a positive value! \n");
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
 void ctob(char car, int pid)
 {
-    int b;
+	int b;
 
-    b = 8;
+	b = 8;
 
-    while (--b >= 0)
-    {
-        if ((car >> b) & 1)
-            kill(pid, SIGUSR1);
-        else if(!((car >> b) & 1))
-            kill(pid, SIGUSR2);
-
-    }
-    usleep(100);
+	while (b--)
+	{
+		if ((car >> b) & 1)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				write (1,"error",5);
+				exit(0);
+			}
+		}
+		else if(!((car >> b) & 1))
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				write (1,"error",5);
+				exit(0);
+			}
+		}
+		usleep(400);
+	}
 }
 
 void    send_mesage(char *msg, int pid)
 {
-    int i;
+	int i;
 
-    i = -1;
-    while (msg[++i])
-        ctob(msg[i], pid);
-    ctob('\0', pid);
+	i = -1;
+	while (msg[++i])
+		ctob(msg[i], pid);
+	// ctob('\0', pid);
 }
 
 int main(int argc, char **argv)
 {
-    int pid;
+	int pid;
 
-    if (check_errors(argc, argv))
-        return 1;
-    pid = ft_atoi(argv[1]);
-    send_mesage(argv[2], pid);
-    return (0);   
+	if (check_errors(argc, argv))
+		return 1;
+	pid = ft_atoi(argv[1]);
+	send_mesage(argv[2], pid);
+	return (0);   
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ouaarabe <ouaarabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:15:37 by ouaarabe          #+#    #+#             */
-/*   Updated: 2023/02/26 08:23:43 by ouaarabe         ###   ########.fr       */
+/*   Updated: 2023/02/26 23:52:17 by ouaarabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,51 @@
 //server :
 void handler(int sig, siginfo_t *info, void *context )
 {
-    static int bit;
-    static int c;
+	static int				bit;
+	static unsigned char	c;
+	static int				pid;
 
-    // printf("handler\n");
-    // if (sig == SIGUSR1)
-    //     c |= 1 << 1;
-    // bit++;
-    // if (bit == 8)
-    // {
-    //     printf("%c", c);
-    //     fflush(stdout);
-    //     c = 0;
-    //     bit = 0;
-    // }
-    // if (sig == SIGUSR2)
-    //     bit++;
-    if (sig == SIGUSR1)
-        write(1, "oumi\n", 5);
-    if (sig == SIGUSR2)
-        write(1, "adam\n", 5);
-
+	if (pid != info->si_pid)
+	{
+		bit = 0;
+		c = 0;
+		pid = info->si_pid;
+	}	
+	if (sig == SIGUSR1)
+	{
+		c |= 128 >> bit;
+		bit++;
+	}
+	if (sig == SIGUSR2)
+		bit++;
+	if (bit == 8)
+	{
+		write (1,&c,1);
+		fflush(stdout);
+		c = 0;
+		bit = 0;
+	}
 }
 
 int main(void)
 {
-    int pid;
+	int pid;
 	struct sigaction sa;
 
-    pid = (int)getpid();
-    ft_printf("here is the process ID : %d\n", pid);
+	pid = (int)getpid();
+	ft_printf("here is the process ID : %d\n", pid);
 	sa.sa_sigaction = handler;
 	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
+	// sigemptyset(&sa.sa_mask);
+	// sigaddset(&sa.sa_mask, SIGUSR1);
+	// sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	// if (sigaction(SIGUSR1, &sa, NULL) == -1)
 	// 	return -1;
 	// if (sigaction(SIGUSR2, &sa, NULL) == -1)
 	// 	return -1;
 
-    while (1)
-        pause();
+	while (1)
+		pause();
 }
